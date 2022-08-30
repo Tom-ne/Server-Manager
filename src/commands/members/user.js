659ade8010cefe1816d12a4dd368eb478d55623f
EliranC07 <109073644+EliranC07@users.noreply.github.com>
2022-08-30 03:16:12 +0300

    Update user.js

--- src/commands/members/user.js
+++ src/commands/members/user.js
@@ -21,33 +21,33 @@ module.exports = {
      * @param {Discord.Client} client
      * @param {Discord.CommandInteraction} interaction
      */
-    run: async(interaction ,client) => {
+    run: async (interaction, client) => {
         const user = await interaction.options.get("member");
 
-        if(!user) {
+        if (!user) {
             var userInfo1 = new EmbedBuilder()
-            .setColor("CYAN")
-            .setThumbnail(interaction.member.displayAvatarURL({ dynamic: true }))
-            .addFields(
-                { name: "__user__", value: `${interaction.member.user.username}`, inline: true},
-                { name: "__userID__", value: `${interaction.member.id}`, inline: true },
-                { name: "__tag__", value: `${interaction.member.user.tag}`, inline: true}
-            )
-            .setFooter({ text: `${interaction.member.tag}`, iconURL: interaction.member.displayAvatarURL({ dynamic: true }) })
-            .setTimestamp()
-            interaction.reply({content: '** **', embeds: [userInfo1]})
+                .setColor("CYAN")
+                .setThumbnail(interaction.member.displayAvatarURL({ dynamic: true }))
+                .addFields(
+                    { name: "__user__", value: `${interaction.member.user.username}`, inline: true },
+                    { name: "__userID__", value: `${interaction.member.id}`, inline: true },
+                    { name: "__tag__", value: `${interaction.member.user.tag}`, inline: true }
+                )
+                .setFooter({ text: `${interaction.member.tag}`, iconURL: interaction.member.displayAvatarURL({ dynamic: true }) })
+                .setTimestamp()
+            interaction.reply({ content: '** **', embeds: [userInfo1] })
         } else {
             var userInfo2 = new EmbedBuilder()
-            .setColor("CYAN")
-            .setThumbnail(user.member.displayAvatarURL({ dynamic: true }))
-            .addFields(
-                { name: "__user__", value: `${user.member.user.username}`, inline: true},
-                { name: "__userID__", value: `${user.member.id}`, inline: true },
-                { name: "__tag__", value: `${user.member.user.tag}`, inline: true}
-            )
-            .setFooter({ text: `${user.member.tag}`, iconURL: user.member.displayAvatarURL({ dynamic: true }) })
-            .setTimestamp()
-            interaction.reply({content: '** **', embeds: [[userInfo2]]})
+                .setColor("CYAN")
+                .setThumbnail(user.member.displayAvatarURL({ dynamic: true }))
+                .addFields(
+                    { name: "__user__", value: `${user.member.user.username}`, inline: true },
+                    { name: "__userID__", value: `${user.member.id}`, inline: true },
+                    { name: "__tag__", value: `${user.member.user.tag}`, inline: true }
+                )
+                .setFooter({ text: `${user.member.tag}`, iconURL: user.member.displayAvatarURL({ dynamic: true }) })
+                .setTimestamp()
+            interaction.reply({ content: '** **', embeds: [[userInfo2]] })
         }
 
     }
